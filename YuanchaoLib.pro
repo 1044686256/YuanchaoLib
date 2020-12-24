a25a41cd6379b93a5b6f2d90c5db44081e368b76
yuanchao.wu <1044686256@qq.com>
2020-12-24 21:29:26 +0800

    1、实现智能指针模板库
    2、实现异常类库

---
+++ YuanchaoLib.pro
@@ -0,0 +1,12 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        Exception.cpp \
+        main.cpp
+
+HEADERS += \
+    Exception.h \
+    SmartPointer.h
---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <debugapi.h>
+#include <smartpointer.h>
+#include "exception.h"
+
+using namespace std;
+using namespace YuanchaoLib;
+
+
+
+int main()
+{
+
+
+    return 0;
+}
+/*class 1:智能指针测试
+class Test
+{
+public:
+    Test()
+    {
+        cout << "Test()" <<endl;
+    }
+    ~Test()
+    {
+        cout << "~Test()" << endl;
+    }
+};
+
+SmartPointer<Test> sp = new Test();
+SmartPointer<Test> nsp ;
+nsp = sp;
+cout << sp.isNull()<<endl;
+cout << nsp.isNull()<<endl;
+*/
+/*
+* class 2:异常类测试
+*    try
+    {
+        THROW_EXCEPTION(NullPointerException,"test");
+        //throw Exception( "test",__FILE__,__LINE__);
+
+    }
+    catch (const Exception& e)
+    {
+        cout << "catch(const Exception& e)"<<endl;
+        cout << e.message()<<endl;
+        cout << e.location()<<endl;
+    }
+*/
