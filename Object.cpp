cde0afdc9bd2b3d0f2bcb4b012703a53368ab2e8
yuanchao.wu <1044686256@qq.com>
2020-12-30 15:20:11 +0800

    YuanchaoLib顶层父类：Object测试完成
    YuanchaoLib顶层父类：Object测试完成

--- Object.cpp
+++ Object.cpp
@@ -1,9 +1,33 @@
 #include "Object.h"
+#include "cstdlib"
+#include "iostream"
 
+using namespace std;
 namespace YuanchaoLib
 {
-Object::Object()
+void* Object :: operator new(unsigned int size)throw()
+{
+    cout << "This is my father class Object malloc"  <<endl;
+    return malloc(size);
+}
+void  Object :: operator delete(void* p)
+{
+     cout << "This is my father class Object free"  <<endl;
+    free(p);
+}
+
+void* Object::operator new[](unsigned int size) throw ()
+{
+    return malloc(size);
+}
+void Object::operator delete[](void* p)
+{
+    free(p);
+}
+Object::~Object()
 {
 
 }
 }
+
+
